22b3b73a351e8d23253682e0ebd43e1e5bd521fa
Tomasz Rosko <tomasz.rosko@gmail.com>
2023-06-21 12:54:56 +0200

    add fetch

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,28 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 //tu maja byc dane o dolarze z maja pobrane od klienta
 function About() {
     const [kurs, setKurs] = useState([])
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('http://api.nbp.pl/api/exchangerates/rates/c/usd/2016-04-04/?format=json')
+            .then((res) => res.json())
+            .then((kurs) => {
+                setKurs(kurs)
+                setLoading(false)
+            })
+    }, [])
+    if (isLoading) return <p>Loading...</p>
+    if (!kurs) return <p>No profile data</p>
     return (
         <article>
-            <h1 className={"title flex justify-center"}>Kurs dolara w maju 2023</h1>
+            <h1 className={"title flex justify-center"}>Kurs {kurs.currency} w maju 2023</h1>
+            <div>
+            </div>
         </article>
     )
 }
@@ -17,9 +32,7 @@ export default About
 About.getLayout = function PageLayout(page) {
     return (
         <>
-            <Header/>
-            {page}
-            <Footer/>
+            <Header/> {page} <Footer/>
         </>
     )
 }
