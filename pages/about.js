36b341ce0ecf5de9495c6ac59d8d26ac4746a7f4
kgneteu <77528950+kgneteu@users.noreply.github.com>
2023-06-21 13:06:17 +0200

    tabela walut

--- pages/about.js
+++ pages/about.js
@@ -1,27 +1,27 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import {useState, useEffect} from "react";
+import {useEffect, useState} from "react";
 
 //tu maja byc dane o dolarze z maja pobrane od klienta
 function About() {
     const [kurs, setKurs] = useState([])
-    const [isLoading, setLoading] = useState(false)
-
     useEffect(() => {
-        setLoading(true)
-        fetch('http://api.nbp.pl/api/exchangerates/rates/c/usd/2016-04-04/?format=json')
-            .then((res) => res.json())
-            .then((kurs) => {
-                setKurs(kurs)
-                setLoading(false)
-            })
+        fetch('https://api.nbp.pl/api/exchangerates/rates/a/usd/2023-05-01/2023-05-31/?format=json)')
+            .then(res => res.json())
+            .then(data => setKurs(data?.rates || []))
     }, [])
-    if (isLoading) return <p>Loading...</p>
-    if (!kurs) return <p>No profile data</p>
     return (
         <article>
-            <h1 className={"title flex justify-center"}>Kurs {kurs.currency} w maju 2023</h1>
-            <div>
+            <h1 className={"title flex justify-center"}>Kurs dolara w maju 2023</h1>
+            <div className={'grid grid-cols-2 mx-auto gap-x-5 '}>
+                {kurs.map((rate, index) => {
+                    return (
+                        <>
+                            <p>{rate?.effectiveDate}</p>
+                            <p>{rate.mid}</p>
+                        </>
+                    )
+                })}
             </div>
         </article>
     )
@@ -32,7 +32,9 @@ export default About
 About.getLayout = function PageLayout(page) {
     return (
         <>
-            <Header/> {page} <Footer/>
+            <Header/>
+            {page}
+            <Footer/>
         </>
     )
 }
