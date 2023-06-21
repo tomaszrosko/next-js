a06bd3869781c6c83f1284b18d8b1ad7c6cbc83d
kgneteu <77528950+kgneteu@users.noreply.github.com>
2023-06-21 11:23:14 +0200

    Kursy dolara po stronie klienta

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,25 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import {useState} from "react";
 
-
+//tu maja byc dane o dolarze z maja pobrane od klienta
 function About() {
-    return <h1 className={"title flex justify-center"}>Witamy w Piekarach w sanktuarium</h1>
-
+    const [kurs, setKurs] = useState([])
+    return (
+        <article>
+            <h1 className={"title flex justify-center"}>Kurs dolara w maju 2023</h1>
+        </article>
+    )
 }
+
 export default About
 
 About.getLayout = function PageLayout(page) {
     return (
         <>
-            <Header />
+            <Header/>
             {page}
-            <Footer />
+            <Footer/>
         </>
     )
 }
